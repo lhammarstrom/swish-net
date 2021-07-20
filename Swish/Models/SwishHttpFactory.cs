f9fad323173401633c8bf36ec6a9ce384d2f945e
Anton Svensson <anton.svensson93@gmail.com>
2021-07-20 11:55:24 +0200

    Test a LOT of stuff

--- Swish/Models/SwishHttpFactory.cs
+++ Swish/Models/SwishHttpFactory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
@@ -16,18 +17,27 @@ namespace Swish.Models
 
         public override HttpMessageHandler CreateMessageHandler()
         {
-            var handler = new HttpClientHandler
-            {
-                SslProtocols = SslProtocols.Tls12,
-                ServerCertificateCustomValidationCallback = (
-                    sender,
-                    certificate,
-                    chain,
-                    sslPolicyErrors) => true,
-                ClientCertificateOptions = ClientCertificateOption.Manual
-            };
+            var handler = new HttpClientHandler();
+            
+            // Got help for this code on https://stackoverflow.com/questions/61677247/can-a-p12-file-with-ca-certificates-be-used-in-c-sharp-without-importing-them-t
+            using X509Store store = new X509Store(StoreName.CertificateAuthority, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
 
-            handler.ClientCertificates.Add(_certificate);
+            var certs = new X509Certificate2Collection();
+            certs.Import(Environment.CurrentDirectory + "\\Swish_Merchant_TestCertificate_1234679304.p12", "swish");
+
+            foreach (X509Certificate2 cert in certs)
+            {
+                if (cert.HasPrivateKey)
+                {
+                    handler.ClientCertificates.Add(cert);
+                }
+                else
+                {
+                    store.Add(cert);
+                }
+            }
+
             return handler;
         }
     }
